e164702e5c7c1dceca8d8c60082e782549707640
Shaw Coney <rockconey@gmail.com>
2023-01-13 17:33:20 -0600

    A commit.

--- script.js
+++ script.js
@@ -2,7 +2,16 @@
 for (let i = 0; i<document.querySelectorAll(".drum").length;i++){
     let audio = new Audio('resources/sounds/tom-1.mp3');
    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-   
-   audio.play()});
+    
+    let audio = new Audio('resources/sounds/crash.mp3');
+    
+    audio.play()});
+    }
 
-}
+
+    function Drum(drumImage, drumSound){
+        this.drumImage = drumImage;
+        this.drumSound = drumSound;
+    }
+
+    let w = new Drum("resources/images/crash.png", "resources/sounds/crash.mp3");
